828333af46f051a845bfefcc61d85e6c6824531c
Mohamed Warsame <mwarsame9@gmail.com>
2017-04-07 12:58:56 -0700

    add arrays for toppings and sizes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,53 @@
-// --Back-end Logic--
-
-function Pizza(size, toppings) {
-  this.size = size;
-  this.toppings = [];
+// Business Logic
+function Pizza(pizzaSizes, meatToppings, nonMeatToppings) {
+  this.pizzaSizes = ["Small", "Medium", "Large"];
+  this.meatToppings = ["Steak", "Chicken", "Beef", "Ham", "Sausage"];
+  this.nonMeatToppings = ["Cheese", "Pineapple", "Tomatoes", "Onions", "Veggies"];
 }
 
-function PizzaToppings(cheese, veggies, pineapple, mushrooms, chicken, steak, beef) {
-  this.cheese = cheese;
-  this.veggies = veggies;
-  this.pineapple = pineapple;
-  this.mushrooms = mushrooms;
-  this.chicken = chicken;
-  this.steak = steak;
-  this.beef = beef;
-}
-
-Pizza.prototype.cost = function() {
-  var pizzaCost = this.toppings.length;
-  this.cost += pizzaCost;
+function Order(pizzaSize, meatTopping, nonMeatTopping) {
+  this.pizzaSize = pizzaSize;
+  this.meatTopping = meatTopping;
+  this.nonMeatTopping = nonMeatTopping;
+  this.cost = 50;
 };
 
-//  --Front-end Logic--
-$(document).ready(function(){
-  $("form#pizzaSize").submit(function(event){
-    event.preventDefault();
-    $("#order").show();
-    $("input:checkbox[name=size]:checked").each(function(){
-      var receipt = $(this).val();
-      $('#order').append(receipt + "<br>");
-    });
-    $('#pizzaSize').hide();
+Order.prototype.receipt = function (){
+  // return "Your total price is: $" + this.cost + ".";
+  return this.pizzaSize + ", " + this.meatTopping ;
+};
+
+Order.prototype.price = function (pizza) {
+  if (this.pizzaSize === "Small"){
+    this.cost -=  4;
+  } else if (this.pizzaSize === "Medium"){
+    this.cost -=  2;
+  } else {
+    this.cost;
+  }
+  if (this.meatTopping === "Steak"){
+    this.cost +=  2;
+  } else {
+    this.cost +=  2;
+  }
+  if (this.customerAge !== "Cheese"){
+   this.cost +=  1;
+  } else {
+    this.cost;
+  }
+  console.log(this.cost);
+};
+
+// User Logic
+$(document).ready(function() {
+  $("#form-one").submit(function(event) {
+  event.preventDefault();
+  var pizzaSizeInput = $("select.pizzaSize").val();
+  var meatToppingInput = $("select.meatToppings").val();
+  var nonMeatToppingInput = $("select.nonMeatToppings").val();
+  var newOrder = new Order(pizzaSizeInput, meatToppingInput, nonMeatToppingInput);
+  var newPizza = new Pizza();
+
+  $("#output").last().append("<h2>" + "Your pizza cost equals: $" + this.cost + "</h2>");
   });
 });
